28cdcf5f2f876546cb16e314879d0db721e1d1ae
JavierS18C <145232643+JavierS18C@users.noreply.github.com>
2024-04-09 22:54:41 -0500

    Add files via upload

---
+++ JavaScrpt deber 1 n2.js
@@ -0,0 +1,81 @@
+// Paso 1: Declara una variable llamada nombre y asígnale tu nombre como valor.
+let nombre = "javier";
+
+// Paso 2: Crea una variable llamada edad y asígnale tu edad.
+let age = "19";
+
+// Paso 3: Declara una variable ‘esEstudiante’ y asígnale un valor booleano.
+let isStudent = true; 
+
+// Paso 4: Crea una variable llamada puntaje y no le asignes ningún valor inicial.
+let score;
+
+// Paso 5: Declara una variable hobbies que contenga un array con tus hobbies.
+let hobbies = ["Leer", "Cocinar", "Correr"];
+
+// Paso 6: Crea un objeto persona que contenga tres propiedades: nombre, edad y esEstudiante.
+let person = {
+    name: "javier",
+    age: 19,
+    isstudent: true
+};
+
+// Paso 7: Declara una variable nullVar y asígnale el valor null.
+let nullVar = null;
+
+// Paso 8: Crea una variable indefinida sin asignarle un valor.
+let undefinedVar;
+
+// Paso 9: Declara una variable numeroHex que contenga un número hexadecimal.
+let numeroHex = 0xFF; // Por ejemplo, 0xFF representa el número hexadecimal 255.
+
+// Paso 10: Crea una variable nombreCompleto que concatene tu nombre y tu apellido.
+let Nombre = "javier";
+let apellido = "saraguro";
+let nombreCompleto = Nombre + " " + apellido;
+
+// Paso 11: Suma dos números y almacena el resultado en una variable suma.
+let num1 = "10";
+let num2 = "20";
+let addition = num1 + num2;
+
+// Paso 12: Resta dos números y guarda el resultado en una variable resta.
+let número1 = "30";
+let número2 = "15";
+let subtraction = número1 - número2;
+
+
+// Paso 13: Multiplica dos números y asigna el resultado a una variable multiplicacion.
+let numero1 = "5";
+let numero2 = "8";
+let multiplicacion = numero1 * numero2;
+
+// Paso 14: Divide dos números y almacena el resultado en una variable division.
+let numeroX = 20;
+let numeroY = 4;
+let division = numeroX / numeroY;
+
+// Paso 15: Usa el operador de módulo para encontrar el residuo de 10 dividido por 3 y almacena el resultado en una variable modulo.
+let module = 10 % 3;
+
+// Paso 16: Incrementa el valor de una variable contador en 1 usando el operador de incremento.
+let counter = 0;
+counter++;
+
+// Paso 17: Decrementa el valor de una variable contadorDec en 1 usando el operador de decremento.
+let counterDec = 10;
+counterDec --;
+
+// Paso 18: Compara dos números para ver si son iguales y almacena el resultado (booleano) en una variable sonIguales.
+let numA = 5;
+let numB = 5;
+let areEqual= numA === numB;
+
+// Paso 19: Comprueba si un número es mayor que otro y guarda el resultado (booleano) en una variable esMayor.
+let numX = 10;
+let numY = 5;
+let isolder = numX > numY;
+
+// Paso 20: Utiliza el operador de asignación compuesta para sumar y asignar un valor a una variable.
+let variable = 5;
+variable += 3; // Esto es equivalente a variable = variable + 3;
